570f613af57189fb051be02baf1fc2a67ba708fa
dgmarie <0001dgmarie@gmail.com>
2022-08-27 13:07:19 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+<div align="center">
+
+# dg-Libadwaita
+
+**A macOS + Yaru configuration for Libadwaita**
+
+This theme is supposed to be used with [this](https://github.com/dgmarie/dg-gnome-theme).
+
+## Credits
+[Yaru](https://github.com/ubuntu/yaru) [WhiteSur-gtk-theme](https://github.com/vinceliuice/WhiteSur-gtk-theme)
---
+++ install.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env bash
+
+color="$1"
+suffix="$2"
+gtk4_dir="${HOME}/.config/gtk-4.0"
+
+# Accent colors
+cp -rf "gtk-4.0/_accent-colors.scss" "gtk-4.0/_accent-colors-temp.scss"
+sed -i "/\$accent_color:/s/orange/${color}/" "gtk-4.0/_accent-colors-temp.scss"
+
+# Install gtk4 configuration
+mkdir -p "${gtk4_dir}"
+rm -rf "${gtk4_dir}/mac-icons" "${gtk4_dir}/gtk.css"
+cp -R "gtk-4.0/assets/mac-icons${suffix}" "${gtk4_dir}/mac-icons"
+sassc -M -t expanded "gtk-4.0/gtk${suffix}.scss" "${gtk4_dir}/gtk.css"
+rm -rf "gtk-4.0/_accent-colors-temp.scss"
+
