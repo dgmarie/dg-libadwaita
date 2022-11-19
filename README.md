35f0b44f66fc8f6f2798d7090b3e01c2414d6258
dgmarie <0001dgmarie@gmail.com>
2022-11-18 19:29:32 -0500

    Rename theme and improve install script

--- README.md
+++ README.md
@@ -1,10 +1,30 @@
 <div align="center">
 
-# dg-Libadwaita
+# qualia Libadwaita theme
 
-**A Yaru inspired configuration for Libadwaita**
+**A Yaru and Libadwaita inspired configuration for Libadwaita**
 
-This theme is supposed to be used with [this](https://github.com/dgsasha/dg-gnome-theme).
+This theme is supposed to be used with [qualia GTK theme](https://github.com/dgsasha/dg-gnome-theme).
+
+</div>
+
+## Installing
+```
+install.sh
+```
+
+OPTIONS:
+	- ` -t <theme_variant>` *optional*
+		- Specify theme variant.
+		- Options: `light` `dark`.
+  - Default: `light`.
+
+	- ` -c <color_name>` *optional*
+		- Specify accent color.
+		- Options: `orange` `bark` `sage` `olive` `viridian` `prussiangreen` `lightblue` `blue` `purple` `magenta` `pink` `red`.
+  - Default: `orange`.
 
 ## Credits
-[Yaru](https://github.com/ubuntu/yaru) [WhiteSur-gtk-theme](https://github.com/vinceliuice/WhiteSur-gtk-theme)
+A huge thanks to:
+- [vinceliuice](https://github.com/vinceliuice) for [WhiteSur-gtk-theme](https://github.com/vinceliuice/WhiteSur-gtk-theme)
+- [lassekongo83](https://github.com/lassekongo83) for [adw-gtk3](https://github.com/lassekongo83/adw-gtk3)
--- install.sh
+++ install.sh
@@ -1,16 +1,61 @@
 #! /usr/bin/env bash
 
-color="$1"
-suffix="$2"
+color_variants=('orange' 'bark' 'sage' 'olive' 'viridian' 'prussiangreen' 'lightblue' 'blue' 'purple' 'magenta' 'pink' 'red')
+theme_variants=('light' 'dark')
+
+color="orange"
+theme="light"
+suffix=""
 gtk4_dir="${HOME}/.config/gtk-4.0"
+
+nc='\033[0m'
+bold='\033[1m'
+red='\033[0;31m'
+bgreen='\033[1;32m'
+
+while getopts 't:c:h' flag; do
+  case "${flag}" in
+  t) theme="${OPTARG}" ;;
+  c) color="${OPTARG}" ;;
+  h)
+    echo "OPTIONS:"
+    echo "  -t <theme_variant>. Set theme variant. [light|dark] (Default: light)"
+    echo "  -c <color_name>. Specify accent color. [orange|bark|sage|olive|viridian|prussiangreen|lightblue|blue|purple|magenta|pink|red] (Default: orange)"
+    echo "  -h Show this message."
+    exit 0
+    ;;
+  *)
+    exit 1
+    ;;
+  esac
+done
+
+if ! printf '%s\0' "${color_variants[@]}" | grep -Fxqz -- "${color}"; then
+  >&2 echo "ERROR: Unrecognized accent color '${color}'."
+  exit 1
+fi
+
+if ! printf '%s\0' "${theme_variants[@]}" | grep -Fxqz -- "${theme}"; then
+  >&2 echo "ERROR: Unrecognized theme variant '${color}'."
+  exit 1
+fi
+
+if [[ "${theme}" == "dark" ]]; then
+  suffix="-dark"
+fi
 
 # Accent colors
 cp -rf "gtk-4.0/_accent-colors.scss" "gtk-4.0/_accent-colors-temp.scss"
 sed -i "/\$accent_color:/s/orange/${color}/" "gtk-4.0/_accent-colors-temp.scss"
 
 # Install gtk4 configuration
-mkdir -p "${gtk4_dir}"
-cp -rf "gtk-4.0/assets/mac-icons/" "${gtk4_dir}"
-sassc -M -t expanded "gtk-4.0/gtk${suffix}.scss" "${gtk4_dir}/gtk.css"
-rm -rf "gtk-4.0/_accent-colors-temp.scss"
-
+if [[ "$(command -v sassc)" ]]; then
+  echo -e "${bgreen}Installing${nc} the ${bold}${color} ${theme} qualia Libadwaita theme ${nc}in ${bold}${gtk4_dir}${nc}"
+  mkdir -p "${gtk4_dir}"
+  cp -rf "gtk-4.0/assets/mac-icons/" "${gtk4_dir}"
+  sassc -M -t expanded "gtk-4.0/gtk${suffix}.scss" "${gtk4_dir}/gtk.css"
+  rm -rf "gtk-4.0/_accent-colors-temp.scss"
+else
+  >&2 echo "ERROR: 'sassc' not found."
+  exit 1
+fi
